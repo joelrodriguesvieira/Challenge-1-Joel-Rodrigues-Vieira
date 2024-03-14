2c654be7ddda234ff9b72370c5adea19041aac12
joelrodriguesvieira <joel2j39@gmail.com>
2024-03-14 12:11:44 -0300

    restructed the code

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,5 +1,5 @@
-let inputUserName = document.getElementById('username');
-inputUserName.addEventListener('input', (e) => addTextDecoration(e));
+let inputUserEmail = document.getElementById('user-email');
+inputUserEmail.addEventListener('input', (e) => addTextDecoration(e));
 
 function addTextDecoration(event) {
     (event.target.value.trim() !== '') ? event.target.classList.add('underline-text') : event.target.classList.remove('underline-text');
